d347286ca21d650da8cb6473e9c5e6a7c69a1618
phemmylintry <phemmylintry@gmail.com>
2021-01-14 22:52:20 +0000

    Deploying to gh-pages from  @ ad36ce67d60d252f39bc345f15411fbf02d290ae 🚀

---
+++ precache-manifest.ada01dfe73fbc7f82ffe39d1b152fbd7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3d459393be322a7740c3556c82c40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2610bc9acdaf544ff0",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "f35065e3ba2b0f8ae526",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "9c6632a80486fe274bf1",
+    "url": "/static/css/main.fffa025b.chunk.css"
+  },
+  {
+    "revision": "b4ffe792179ef3e54975",
+    "url": "/static/js/2.7422df90.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.7422df90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2610bc9acdaf544ff0",
+    "url": "/static/js/3.cd519421.chunk.js"
+  },
+  {
+    "revision": "f35065e3ba2b0f8ae526",
+    "url": "/static/js/4.ee867490.chunk.js"
+  },
+  {
+    "revision": "9c6632a80486fe274bf1",
+    "url": "/static/js/main.b1ecf023.chunk.js"
+  },
+  {
+    "revision": "469703559486e830279d",
+    "url": "/static/js/runtime-main.431eb4b6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "fdd011c038f5f29a62f703e15c1c5888",
+    "url": "/static/media/email.fdd011c0.PNG"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "9077b527063e3f7f6a186c0b0602bfb4",
+    "url": "/static/media/idpm.9077b527.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "9426ce0bfab529cf2b41c0e28c80d7fd",
+    "url": "/static/media/nursery.9426ce0b.PNG"
+  },
+  {
+    "revision": "4cc0cc952254c5bce9679b4834a9cd53",
+    "url": "/static/media/oou.4cc0cc95.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developerFolio/precache-manifest.4b33d9235aca02fde1a923bab5d1672c.js"
+  "/precache-manifest.ada01dfe73fbc7f82ffe39d1b152fbd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
